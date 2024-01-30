68bb1be99406bd7215e6073ac55a1684b13ff8a2
Jammi Dee <jammi_dee@yahoo.com>
2024-01-30 15:55:45 +0800

    Added SQLite linrary

---
+++ src/libs/ladatabase.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2024 Lalulla, Inc. All rights reserved.
+ * Copyright (c) 2024 - Joel M. Damaso - mailto:jammi_dee@yahoo.com Manila/Philippines
+ * This file is part of Lalulla System.
+ * 
+ * LaKuboTron Framework is distributed under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * LaKuboTron System is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Lalulla System.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Framework Designed by: Jammi Dee (jammi_dee@yahoo.com)
+ *
+ * File Create Date: 01/30/2024 3:52PM
+ * Created by: Jammi Dee
+ * Modified by: Jammi Dee
+ *
+*/
+
+const sqlite3   = require('sqlite3').verbose();
+const path      = require('path');
+const dbPath    = path.resolve(__dirname, 'lalulla.db');
+
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error(err.message);
+        throw err;
+    }
+    console.log('Connected to the expenses database.');
+});
+
+db.run(`CREATE TABLE IF NOT EXISTS expenses (id INTEGER PRIMARY KEY AUTOINCREMENT, amount REAL)`);
+
+module.exports = db;
